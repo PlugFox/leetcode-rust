692a8d6e5fa69af2fdcd8b73110cb99700d89558
Mike Matiunin <plugfox@gmail.com>
2025-03-24 07:08:56 +0400

    Rename main binary to hello and add a new hello.rs file with a Hello World program

--- bin/main.rs
+++
@@ -1,7 +0,0 @@
-// cargo run
-// rustc -O main.rs -o main.run && ./main.run && rm main.run
-// rustc -C opt-level=3 -C lto=fat -C codegen-units=1 -C panic=abort \
-//  -C strip=symbols main.rs -o main.run && ./main.run && rm main.run
-fn main() {
-    println!("Hello, LeetCode!");
-}
