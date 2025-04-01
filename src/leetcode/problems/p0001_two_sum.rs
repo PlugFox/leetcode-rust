0ee34b9632ebd2a6a7584dadb34c53ae288dfa66
Mike Matiunin <plugfox@gmail.com>
2025-04-01 07:21:30 +0400

    Refactor Two Sum implementation to use a brute-force approach and update return value for no solution case

--- src/leetcode/problems/p0001_two_sum.rs
+++ src/leetcode/problems/p0001_two_sum.rs
@@ -33,18 +33,16 @@ impl Solution {
     // Add implementations of LeetCode problems here
     // Example for Two Sum (problem 1):
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        use std::collections::HashMap;
-        let mut map = HashMap::new();
-
-        for (i, &num) in nums.iter().enumerate() {
-            let complement = target - num;
-            if let Some(&j) = map.get(&complement) {
-                return vec![j as i32, i as i32];
+        for i in 0..nums.len() {
+            let a = nums[i];
+            for j in (i + 1)..nums.len() {
+                let b = nums[j];
+                if a + b == target {
+                    return vec![i.try_into().unwrap(), j.try_into().unwrap()];
+                }
             }
-            map.insert(num, i);
         }
-
-        vec![]
+        vec![-1, -1] // If no solution is found, return an invalid result
     }
 }
 
